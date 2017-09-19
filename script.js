67c35f203621250ef14f400182f503afdb7ffc70
Tommiee <tomdepom1999@gmail.com>
2017-09-19 15:39:03 +0200

    Rekengame-0.4

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
-console.log(getProblem());
-
 const problem = document.getElementById('problem');
+const input = document.getElementById('myInput');
+const feedback = document.getElementById('feedback');
 
-problem.innerHTML = getProblem();
+let prob1 = getNumber();
+let prob2 = getNumber();
+let correct = prob1 * prob2;
 
-function getProblem(){
-  let ans = getNumber() + "*" + getNumber();
-  return ans;
-}
+problem.innerHTML = prob1 + "x" + prob2;
+
+document.addEventListener('keydown',(e)=>{
+  console.log(e.keyCode);
+  if(e.keyCode == 13){
+    let inp = input.value;
+    if(inp == correct){
+      feedback.innerHTML = "Correct!";
+    } else {
+      feedback.innerHTML = "Fout!";
+    }
+  }
+
+})
+
+// function getProblem(){
+//   let ans = getNumber() + "*" + getNumber();
+//   return ans;
+// }
 
 function getNumber(){
   return Math.floor(Math.random()*9)+1;
 }
+
+console.log(prob1 + "x" + prob2 + "=" + correct);

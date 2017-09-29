7362d621bfd2e86c1153248b010f9fdd732b245f
Tommiee <tomdepom1999@gmail.com>
2017-09-29 16:36:57 +0200

    Rekenspel_2.0

--- script.js
+++ script.js
@@ -6,47 +6,72 @@ const level = document.getElementById('level');
 
 let point = 0;
 let lvlup = 0;
-let lvl = 0;
+let lvl = 1;
 let prob1, prob2, correct;
-
-function getQuestion(){
-  prob1 = getNumber();
-  prob2 = getNumber();
-  correct = prob1 * prob2;
-  problem.innerHTML = prob1 + "x" + prob2;
-  console.log(prob1 + "x" + prob2 + "=" + correct);
-}
 
 getQuestion();
 
 document.addEventListener('keydown',(e)=>{
   if(e.keyCode == 13){
     let inp = input.value;
-    if(inp == correct){
-      feedback.innerHTML = "Correct!";
-      feedback.style.color = "green";
-      point++;
-      lvlup++;
-      if(lvlup >= 10){
-        lvl++;
-        lvlup=0;
-      }
-      score.innerHTML = "score: " + point;
-      level.innerHTM = "level: " + lvl;
-      getQuestion();
-    } else {
-      feedback.innerHTML = "Fout!";
-      feedback.style.color ="red";
-      getQuestion();
+    switch (lvl) {
+      case 1:
+        if(inp == correct){
+          checkAns(true);
+        } else {
+          checkAns(false);
+        }
+      break;
+      case 2:
+        if(inp == prob2){
+          checkAns(true);
+        } else {
+          checkAns(false);
+        }
+      break;
     }
   }
 })
 
-// function getProblem(){
-//   let ans = getNumber() + "*" + getNumber();
-//   return ans;
-// }
-
 function getNumber(){
   return Math.floor(Math.random()*9)+1;
+}
+
+function getQuestion(){
+  prob1 = getNumber();
+  prob2 = getNumber();
+  correct = prob1 * prob2;
+  console.log(prob1 + "x" + prob2 + "=" + correct);
+  switch (lvl) {
+    case 1:
+      problem.innerHTML = prob1 + "x" + prob2 + "=" + "?";
+    break;
+    case 2:
+      problem.innerHTML = prob1 + "x" + "?" + "=" + correct;
+    break;
+  }
+}
+
+function checkAns(c){
+  if(c){
+    feedback.innerHTML = "Correct!";
+    feedback.style.color = "green";
+    point++;
+    lvlup++;
+    console.log(lvlup);
+    if(lvlup >= 5){
+      if(lvl < 4){
+        lvl++;
+      }
+      lvlup=0;
+    }
+    score.innerHTML = "score: " + point;
+    level.innerHTML = "level " + lvl;
+    getQuestion();
+  } else {
+    feedback.innerHTML = "Fout!";
+    feedback.style.color ="red";
+    getQuestion();
+  }
+  return;
 }
